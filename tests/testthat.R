68abbdb4676e6ce8ba500d7a4e079160072c3e31
young-geun <dudrmshub@gmail.com>
2019-12-03 10:54:19 +0900

    testthat

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(youngtool)
+
+test_check("youngtool")
---
+++ tests/testthat/test-mc_optimism.R
@@ -0,0 +1,19 @@
+context("Optimism")
+
+test_that(
+  "Optimism with MC simulation",
+  {
+    mc_sample <- mc_data(rnorm, 10, 50)
+    # y = 1 + 3 * x + eps
+    insample <- compute_insample(
+      mc_sample,
+      5,
+      function(x) {1 + 3 * x},
+      rnorm,
+      lm,
+      y ~ x
+    )
+    expect_s3_class(insample, c("data.table", "data.frame"))
+    expect_named(insample, c("training", "insample", "optimism"))
+  }
+)
